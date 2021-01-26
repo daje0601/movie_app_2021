bbe0c814440554b3f1ba7679bbcc304061a79400
daje0601 <englishmt4118@gmail.com>
2021-01-26 15:42:51 +0900

    complete css

--- src/Movie.js
+++ src/Movie.js
@@ -5,14 +5,20 @@ import "./Movie.css";
 // 이 경우에는 movies component가 필요하지 않다고 하는데 왜?
 
 
-function Movie({ id, year, title, summary, poster}) {
+function Movie({ id, year, title, summary, poster, genres}) {
     return (
         <div class="movie">
             <img src={poster} alt={title} title={title} />
             <div class="movie__data">
                 <h3 class="moive__title">{title}</h3>
                 <h5 class="moive__year">{year}</h5>
-                <p class="moive__summary">{summary}</p>
+                <ul className="genres">
+                    {genres.map((genre, index) => (
+                        <li key={index} className="genres__genre">{genre}</li>
+                    ))}
+                </ul>
+                <p class="moive__summary">{summary.slice(0, 140)}...</p>
+                
             </div>
         </div>
     )
@@ -24,6 +30,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Movie;
